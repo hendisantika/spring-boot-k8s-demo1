9c52787b53b59aec196ad9bfea2fb1658388b63a
Hendi Santika <hendisantika@yahoo.co.id>
2021-01-01 19:08:55 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootk8sdemo1/SpringBootK8sDemo1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootk8sdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootK8sDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootK8sDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootk8sdemo1/SpringBootK8sDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootk8sdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8sDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
